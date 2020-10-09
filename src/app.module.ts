f28860840b7b997518c650c29321f0260f040b85
CesarHernandez90 <infocesar03@gmail.com>
2020-10-08 23:26:52 -0500

    [Config] Se configuró el ConfigService de nest para el uso de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost/product')],
+  imports: [
+    ProductModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
