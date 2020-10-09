7391e5fcf4b0f726a7d5fc55bb95ce7795130d38
CesarHernandez90 <infocesar03@gmail.com>
2020-10-08 23:46:12 -0500

    [Install] Se agregó y configuró el uso de Swagger para la documentación

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Products')
+    .setDescription('Descripción')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      displayRequestDuration: true,
+    },
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port);
